e58a4d877b301358b7922e1b9cdbab87cda4a651
jorgeb1980 <jbaugracia@gmail.com>
2015-06-01 20:32:02 +0200

    du clone
    Initial upload of files (everything auto-generated by VC++ 2010)

---
+++ du/du/du.cpp
@@ -0,0 +1,11 @@
+// du.cpp: archivo de proyecto principal.
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine(L"Hola a todos");
+    return 0;
+}
